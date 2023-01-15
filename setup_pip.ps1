4b961ef79ff967140f0ad56752d94895660fb695
MatteoH2O1999 <matteo.dellacqua2@studenti.unimi.it>
2023-01-15 18:07:53 +0100

    Add logs to setup pip

--- setup_pip.ps1
+++ setup_pip.ps1
@@ -2,13 +2,17 @@ $Version = $env:PYTHON_VERSION
 $PythonPath = $env:SETUP_PYTHON_PATH
 $Semver = $Version.Split('.')
 $pip = 0
+Write-Output "Setting up pip for python $Version in path $PythonPath"
 if ([int]$Semver[0] -eq 3 -and [int]$Semver[1] -ge 4) {
+    Write-Output "Using ensurepip..."
     Invoke-Expression -Command "$PythonPath -m ensurepip"
     if ([int]$Semver[1] -gt 4) {
+        Write-Output "Updating pip..."
         Invoke-Expression -Command "$PythonPath -m pip install --upgrade pip"
     }
     $pip = 1
 } elseif ([int]$Semver[1] -ge 3) {
+    Write-Output "Using get_pip.py..."
     $GetPipFile = New-TemporaryFile
     Invoke-WebRequest -Uri 'https://bootstrap.pypa.io/pip/3.3/get-pip.py' -OutFile $GetPipFile
     Invoke-Expression -Command "$PythonPath $GetPipFile"
@@ -17,7 +21,9 @@ if ([int]$Semver[0] -eq 3 -and [int]$Semver[1] -ge 4) {
 }
 if ($pip) {
     if ([int]$Semver[0] -eq 3 -and [int]$Semver[1] -lt 5) {
+        Write-Output "Installing typing for old Python versions..."
         Invoke-Expression -Command "$PythonPath -m pip install typing"
     }
+    Write-Output "Installing wheel and setuptools..."
     Invoke-Expression -Command "$PythonPath -m pip install --upgrade wheel setuptools"
 }
